bf715f917a77c2f0c54610fa8572053f5123cdd2
UmerSethi <m.umersethi91@gmail.com>
2023-01-20 15:26:35 +0500

    first commit

---
+++ Functions/app.js
@@ -0,0 +1,139 @@
+// username = "Jaffar Aman";
+ 
+// function foo(){
+//     console.log(username)
+// }
+
+// foo()
+
+
+
+
+// -------------------------
+
+
+
+
+// function foo(){
+//     username = "Jaffar Aman";
+//     console.log(username)
+// }
+
+// foo()
+
+
+// -------------------------
+
+
+
+// function foo(){
+//     username = "Jaffar Aman";
+// }
+// console.log(username)
+
+// foo()
+
+
+// -------------------------
+
+
+                                        // =======================
+                                        //    LOCAL VARIABLE :-
+                                        // =======================
+
+// function foo(){
+//     var username = "Jaffar Aman";
+//     console.log(username)
+// }
+
+// foo()
+
+
+
+// -------------------------
+
+
+                                        // =======================
+                                        //    GLOBAL VARIABLE :-
+                                        // =======================
+
+// var username = "Jaffar Aman";
+
+// function foo(){
+//     console.log(username)
+// }
+
+// foo();
+
+
+// -------------------------
+// -------------------------
+ 
+                           
+                            // ===============================================================
+                            //    EXIBITION CONTEXT OF JAVASCRIPT (OR) TECHNICAL QUESTIONS :-
+                            // ===============================================================
+
+
+// var username = "Jaffar Aman";
+
+// function foo(){
+//     username = "Talha"
+//     console.log(username)
+// }
+
+// foo();
+
+
+// -------------------------
+
+
+
+
+// var username = "Jaffar Aman";
+
+// function foo(){
+//     username = "Talha"
+//     console.log("1st" , username)
+// }
+
+// console.log( "2nd",username)
+
+// foo();
+
+
+// -------------------------
+
+
+
+// var username = "Jaffar Aman";
+
+// function foo(){
+//     username = "Talha"
+//     console.log("1st" , username)
+// }
+
+
+// foo();
+
+// console.log( "2nd",username)
+
+
+
+// -------------------------
+
+
+// var username = "Jaffar Aman";
+
+// function foo(){
+//    var username = "Talha"
+//     console.log("1st" , username)
+// }
+
+
+// foo();
+
+// console.log( "2nd",username)
+
+
+// -------------------------
