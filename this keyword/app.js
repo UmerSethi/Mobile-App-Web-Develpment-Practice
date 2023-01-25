532362e96a533d48b45062137dbe5a297df3ab05
UmerSethi <m.umersethi91@gmail.com>
2023-01-25 18:47:29 +0500

    commit

--- this keyword/app.js
+++ this keyword/app.js
@@ -129,27 +129,129 @@
 //                                    WHY IT IS CALLED "this" ?
 
 
-let laptop1 = {
-        cpu: 'i9',
-        ram: 16,
-        brand: "HP",
+// let laptop1 = {
+//         cpu: 'i9',
+//         ram: 16,
+//         brand: "HP",
     
-        getConfig: function(){
-           console.log(this.cpu)
-        }
-    }
-    
-    
-    let laptop2 = {
-        cpu: 'i7',
-        ram: 16,
-        brand: "Apple",
-    
-        getConfig: function(){
-           console.log(this.cpu)
-        }
-    }
+//         getConfig: function(){
+//            console.log(this.cpu)
+//         }
+//     }
     
     
-    laptop1.getConfig()
+//     let laptop2 = {
+//         cpu: 'i7',
+//         ram: 16,
+//         brand: "Apple",
     
+//         getConfig: function(){
+//            console.log(this.cpu)
+//         }
+//     }
+    
+    
+
+//    if(laptop1.cpu > laptop2.cpu){
+//       console.log(laptop1);
+//    }
+//    else{
+//      console.log(laptop2)
+//    }
+
+
+
+
+
+
+
+//                            =========================================
+
+
+
+
+// function getFastLaptop(Laptop1, Laptop2){
+   
+//    if(laptop1.cpu > laptop2.cpu){
+//       console.log(laptop1);
+//    }
+//    else{
+//    console.log(laptop2)
+//    }
+
+// }
+
+
+//    let laptop1 = {
+//          cpu: 'i9',
+//          ram: 16,
+//          brand: "HP",
+      
+//          getConfig: function(){
+//             console.log(this.cpu)
+//          }
+//       }
+      
+      
+//       let laptop2 = {
+//          cpu: 'i7',
+//          ram: 16,
+//          brand: "Apple",
+      
+//          getConfig: function(){
+//             console.log(this.cpu)
+//          }
+//       }
+      
+      
+
+//       getFastLaptop(laptop1,laptop2);
+
+
+
+
+//                         X   =========================================   X
+
+
+
+
+            
+      let laptop1 = {
+      cpu: 'i9',
+      ram: 16,
+      brand: "HP",
+                     
+      compare: function(other){
+      if(this.cpu > other.cpu)
+      console.log(this)
+      else
+      console.log(other)
+         }
+         ,
+            
+         getConfig: function(){
+         console.log(this.cpu)
+         }
+      }
+                  
+                  
+      let laptop2 = {
+         cpu: 'i7',
+      ram: 16,
+      brand: "Apple",
+
+      compare: function(other){
+         if(this.cpu > other.cpu)
+         console.log(this)
+         else
+         console.log(other)
+      },
+
+      getConfig: function(){
+         console.log(this.cpu)
+      }
+      }           
+                  
+
+
+      laptop2.compare(laptop1)
